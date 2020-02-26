4dce7841f6e15b288b71696c623a0714205bf832
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-02-26 18:33:29 +0530

    XAMARIN-33590 - Sample added

---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ScheduleXamarin
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("SchedulerPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<SchedulerPage, SchedulerViewModel>();
+        }
+    }
+}
